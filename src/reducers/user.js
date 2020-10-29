d79391d3d2991efddcc230456d4c019f996a7208
Leticia <leticia.bora@gmail.com>
2020-10-29 07:04:59 -0300

    teste

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  user: []
+}
+
+const setUser = (state = initialState, action) => {
+  switch(action.type) {
+
+  }
+}
+
+export default setUser;
